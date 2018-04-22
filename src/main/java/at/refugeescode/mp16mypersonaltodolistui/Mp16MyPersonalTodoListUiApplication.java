43ab943d572b25ca97e8b014cdf5c31d4281942b
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-22 13:22:14 +0200

    add spring structure

---
+++ src/main/java/at/refugeescode/mp16mypersonaltodolistui/Mp16MyPersonalTodoListUiApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp16mypersonaltodolistui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp16MyPersonalTodoListUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp16MyPersonalTodoListUiApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp16mypersonaltodolistui/Mp16MyPersonalTodoListUiApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp16mypersonaltodolistui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp16MyPersonalTodoListUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
